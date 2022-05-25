7fcb9dbebdff2b88e2d0b0e3dc35a72e94e2334d
developerdiary <ikhlaquesoft@gmail.com>
2022-05-25 19:33:30 +0530

    Dot ENV Module

--- api/server.js
+++ api/server.js
@@ -1,53 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
+const url = process.env.MONGO_DB_URL;
 
-const url = 'mongodb+srv://developerdiary:zQpxtgQA2v7LbS3z@cluster0.qfl35.mongodb.net/test?retryWrites=true&w=majority';
-
-
-mongoose.connect(url, {
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then( () => console.log("Connected to DB") )
-    .catch(console.error);
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
 
+const Todo = require("./models/Todo");
 
-
-const Todo = require('./models/Todo');
-
-app.get('/todos', async(req, res) => {
-    const todos = await Todo.find();
-    res.json(todos);
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
 });
 
-app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-        text: req.body.text
-    })
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
 
-    todo.save();
+  todo.save();
 
-    res.json(todo);
+  res.json(todo);
 });
 
-app.delete('/todo/delete/:id', async(req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-    res.json(result);
-})
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
 
-app.get('/todo/complete/:id', async(req, res) => {
-    const todo = await Todo.findById(req.params.id);
-    todo.complete = !todo.complete;
-    todo.save();
-    res.json(todo);
-})
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
+  todo.save();
+  res.json(todo);
+});
 
-
-app.listen(3001, () => console.log(" Server started on Port 3001") );
+app.listen(3001, () => console.log(" Server started on Port 3001"));
